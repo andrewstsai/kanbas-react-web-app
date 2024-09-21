dc68230ab991aa2860df94882915b229f4eb6695
Andrew <tsai.an@northeastern.edu>
2024-09-21 19:19:00 -0400

    initial commit

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -4,7 +4,11 @@ export default function CourseStatus() {
       <h2>Course Status</h2>
       <button>Unpublish</button> <button>Publish</button>
       <button>Import Existing Content</button>
-      {/* Complete on your own */}
+      <button>Import From Commons</button>
+      <button>Choose Home Page</button>
+      <button>View Course Stream</button>
+      <button>New Announcement</button>
+      <button>New Analytics</button>
       <button>View Course Notifications</button>
     </div>
   );
